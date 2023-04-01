c8bd16893820b49dc7344fb67ddbd99e49d34398
gzf <122297332+Argzf@users.noreply.github.com>
2023-04-01 21:04:51 +0300

    Add files via upload

---
+++ Rps.py
@@ -0,0 +1,55 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_image = [rock, paper, scissors]
+
+user_choice = int(input("WHAT DO YOU CHOOSE? TYPE 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+print(game_image[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_image[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("YOU TYPED AN INVALID NUMBER, YOU LOSE!")
+
+elif user_choice == 0 and computer_choice == 2:
+    print("YOU WIN!")
+
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+
+elif computer_choice > user_choice:
+    print("You lose")
+
+elif user_choice > computer_choice:
+    print("You win!")
+
+elif computer_choice == user_choice:
+  print("It's a draw")
